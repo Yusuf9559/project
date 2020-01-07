0a240c11063ae4f4b229bf30760cdb2e02b3b2fd
unknown <siddiqov95@list.ru>
2020-01-07 20:03:12 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const a = 5;
+
+console.log(a);
+
+console.log(4/0);
+console.log('String'*9);
+var string = "hello";
+let something;
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried:false
+};
+console. log(persone["name"]);
+
+let arr = ['plum.png','orange.jnp','apple.bmp'];
+
+console.log(arr[0]);
+
+//let answer = +prompt("ti fey?","yes");
+
+//console.log(typeof(answer))
+
+//console.log("arr" + " - object");
+//console.log(4 + + " - object");
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log("2" ===2);
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose);    
